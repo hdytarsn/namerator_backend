f27a9532a842eb890daf412b9f22327c04ccc557
Hidayet Arasan <info@hidayetarasan.com>
2021-03-26 04:51:31 +0300

    authentication/ login route and functions added

--- routes/api.php
+++ routes/api.php
@@ -20,5 +20,22 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
+Route::post('/login', function (Request $request) {
+    $data = $request->validate([
+        'email' => 'required|email',
+        'password' => 'required'
+    ]);
+    $user = User::where('email', $data['email'])->first();
+    if (!$user || !Hash::check($data['password'], $user->password)) {
+        return response([
+            'message' => ['These credentials do not match our records.']
+        ], 422);
+    }
+    $token = $user->createToken($user->name.'token')->plainTextToken;
+    $response = [
+        'user' => $user,
+        'token' => $token
+    ];
+    return response($response, 201);
+});
 
